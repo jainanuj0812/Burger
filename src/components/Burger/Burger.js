1e2400e25727e29bd9764526dbd3bc9ed475a53c
Jain <Anuj.Jain@mastercard.com>
2017-12-07 17:46:56 +0530

    Build Controllers, WIP modal

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,14 +5,14 @@ import BurgerIngredient from './BurgerIndegreint/BurgerIngredient'
 const burger = (props) => {
     let transformedIng = Object.keys(props.ingredients)
         .map(igKey => {
+            console.log(props.ingredients[igKey]);
              return [...Array(props.ingredients[igKey])].map((_, i) => {
                  return <BurgerIngredient key={igKey + i} type={igKey}/>
             });
         }).reduce((arr, ele) => {
             return arr.concat(ele);
         }, []);
-
-    if (transformedIng) {
+    if (transformedIng.length === 0) {
         transformedIng = <p>Please start adding ingredients</p>
     }
     return (
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './Layout.css';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux_';
 
 const layout = (props) => (
     <Aux>
